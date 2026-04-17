913c65f920aa5828a78210294bdfe19e92d8f0bc
Nehlson <nelsondennis350@gmail.com>
2026-04-17 11:08:32 +0100

    updated core and crud

--- app/core/security.py
+++ app/core/security.py
@@ -1,17 +1,27 @@
 from datetime import datetime, timedelta
+import os
 from jose import jwt
+from dotenv import load_dotenv
+
+#Load my .env data into this environment
+load_dotenv()
+
+#get data from my .env
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 
-
-SECRET_KEY = "2345TFDFASF"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
+#Create access token for logged in user
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.utcnow() + timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    #update the data dictionary to include expire or add iat time stamp
     to_encode.update({"exp": expire})
 
+    #Generate jwt signed token
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    
     return encoded_jwt
